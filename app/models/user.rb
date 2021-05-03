490d014e52422bc03393e50889313fe006596797
ark55555 <ark.tooyoung@gmail.com>
2021-05-03 23:59:39 +0000

    [Add]Practical4userindex

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,21 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  
+  def following?(other_user)
+    passive_relationships.find_by(following_id:other_user.id)
+  end
+
+  def follow!(other_user)
+    passive_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    active_relationships.find_by(following_id: other_user.id).destroy
+  end
 
 
   attachment :profile_image, destroy: false
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update] do
-    resource :relationships, only: [:create, :destroy]
+    member do
+      get :following, :followers
+    end
   end
+  resources :relationships, only: [:create, :destroy]
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
