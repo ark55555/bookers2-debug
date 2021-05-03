2f2af8e4626ae2a482c1d80a9eabdf17f0d94fd6
ark55555 <ark.tooyoung@gmail.com>
2021-05-03 10:13:40 +0000

    [Start]Practical4

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'top' => 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
