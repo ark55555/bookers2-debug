dcc26385d9441d6bc15eb0294bb5a86f1db14218
ark55555 <ark.tooyoung@gmail.com>
2021-07-12 04:43:50 +0000

    [Add]chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :chats
+  has_many :user_rooms
+  has_many :rooms, through: :user_rooms
 
   include JpPrefecture
   jp_prefecture :prefecture_code
